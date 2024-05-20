c8e631a03da90452c3b20b35cde15577ee05185e
AyobamiH <heryourbarme@live.com>
2024-05-20 08:22:34 +0100

    vite updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+  
+// });
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // Ensure client-side routing works
+    proxy: {
+      '/api': {
+        target: 'https://skills2profits.onrender.com/',  // Your backend server
+        changeOrigin: true,
+        secure: false,
+      },
+    
+    },
+    port: 5173,
+    
+  },
   
+
 });
+
+
+
