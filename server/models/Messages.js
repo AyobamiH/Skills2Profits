84617d92d62950343c57c4cb637086a821e6413e
AyobamiH <heryourbarme@live.com>
2024-06-29 14:57:48 +0100

    Model

--- server/models/Messages.js
+++ server/models/Messages.js
@@ -5,6 +5,7 @@ const messageSchema = new mongoose.Schema({
   email: { type: String, required: true },
   phone: { type: String, required: true },
   message: { type: String, required: true },
+  country: { type: String, required: true },
   agreement: { type: Boolean, required: true },
   createdAt: { type: Date, default: Date.now }
 });
