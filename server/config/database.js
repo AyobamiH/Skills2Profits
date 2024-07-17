a292f93d2bc330f3c05bd79bcf868c268f6dcf00
AyobamiH <heryourbarme@live.com>
2024-07-17 14:19:46 +0100

    Add project dependencies and scripts in package.json for Express server setup
    - Defined project dependencies including Express, Mongoose, CORS, Dotenv, and Passport for backend functionality.
    - Included nodemon for development to enable automatic server restarts on file changes.
    - Configured npm scripts:
      - start to run the server using Node.js.
      - dev to run the server in development mode using nodemon.
    - Set up devDependencies to ensure proper development environment configuration.

--- server/config/database.js
+++ server/config/database.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    // console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     // console.error(err);
     process.exit(1);
