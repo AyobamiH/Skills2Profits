c271633dc471408fd07563e7e7bb0ea5059dd9f6
AyobamiH <heryourbarme@live.com>
2024-08-07 12:41:19 +0100

    About Section

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -8,7 +8,7 @@ const AboutSection = () => {
  
       <div id="about" className="container mx-auto px-4">
         <motion.div
-          className="flex flex-col  md:flex-row items-center"
+          className="flex flex-col md:flex-row items-center"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.8 }}
@@ -20,6 +20,7 @@ const AboutSection = () => {
             transition={{ duration: 0.8 }}
           >
             <h2 className="text-3xl font-bold text-[#001f3f] mb-4">About</h2>
+            <h4 className="text-2xl font-normal w-fit text-[#f1c40f] bg-[#001f3f] mb-4">Bridging The Gap!</h4>
             <p className="text-[#001f3f] mb-4">
               The mission is to bridge the gap between talent and opportunity by offering a sponsorship program to foster financial growth and development, ensuring that talented individuals have the chance to thrive in the competitive world of trading. 
             
