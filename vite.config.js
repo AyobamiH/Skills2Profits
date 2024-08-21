98f6aa9d0f3e52cce1660c4a3ba83e05f3931470
AyobamiH <heryourbarme@live.com>
2024-08-21 04:32:30 +0100

    Configure Vite server with React plugin and API proxy
    - Set up Vite configuration with React plugin for optimized development workflow.
    - Configured the development server to proxy API requests to 'http://localhost:4545' for seamless backend integration.
    - Ensured proper handling of cross-origin requests by enabling  and disabling .
    - Set the development server to run on port 5174 for consistency with the project setup.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4545',  // Your backend server
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    port: 5174, // Ensure this matches the port where your Vite dev server is running
+  },
 })
