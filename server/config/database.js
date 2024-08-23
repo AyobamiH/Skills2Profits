077ff447f25937b61dc5b8dc40f598d0838fe505
AyobamiH <heryourbarme@live.com>
2024-08-23 05:47:13 +0100

    database update

--- server/config/database.js
+++ server/config/database.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    // console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err);
+    // console.error(err);
     process.exit(1);
   }
 };
