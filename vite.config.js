b1bf9a2fb8976c8cdd919f0858d993f991c1483d
AyobamiH <heryourbarme@live.com>
2024-08-28 21:23:44 +0100

    another updates

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://skills2profits.onrender.com',  // Your backend server
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-    port: 5174, // Ensure this matches the port where your Vite dev server is running
-  },
+  
 });
