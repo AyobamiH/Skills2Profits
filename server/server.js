8373ba1b47974330a9436e36230044d9cd8ee170
AyobamiH <heryourbarme@live.com>
2024-08-28 20:19:56 +0100

    updates

--- server/server.js
+++ server/server.js
@@ -11,20 +11,14 @@ app.use(express.json());
 
 
 // Enable CORS so that the frontend (localhost:5174) can communicate with the backend (localhost:4545)
-const corsOptions = {
-  origin: 'https://your-frontend-domain.vercel.app', // Your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true, // If you need to send cookies
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors())
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+  origin: 'https://skills2profits.com', 
+}))
 // Use the messages routes for the '/messages' path
 app.use('/api', messagesRoutes);
-
+app.get('/', (req, res) => {
+  res.send('Welcome to the API server!');
+});
 // Verify MongoDB URI
 connectDB();
 
--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: ['http://localhost:4545','https://skills2profits.onrender.com'],  // Your backend server
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//     port: 5174, // Ensure this matches the port where your Vite dev server is running
-//   },
-// })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://skills2profits.onrender.com',  // Your backend server URL
+        target: 'https://skills2profits.onrender.com',  // Your backend server
         changeOrigin: true,
         secure: false,
       },
     },
-    // port: 5174, // This is only for local development
+    port: 5174, // Ensure this matches the port where your Vite dev server is running
   },
 });
