0d845e6bfc4c314b160ef081c9551e5fe4dd05cb
AyobamiH <heryourbarme@live.com>
2024-08-28 17:08:25 +0100

    vite update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4545',  // Your backend server
+        target: 'https://skills2profits.onrender.com/api/:path*',  // Your backend server
         changeOrigin: true,
         secure: false,
       },
